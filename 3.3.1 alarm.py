2c7729ca9dc032d2de9af990f2955556de50f579
Michael H <michael.hevi@vtsd.com>
2024-05-01 13:09:22 -0400

    edcfdev

---
+++ 3.3.1 alarm.py
@@ -0,0 +1,27 @@
+Attendance = []
+
+Attendance.append("zaine")
+Attendance.append("zay")
+Attendance.append("zuko")
+Attendance.append("zofia")
+Attendance.append("zander")
+Attendance.append ("zack")
+Attendance.append("ben")
+Attendance.append("boolbilly")
+Attendance.append("booboo")
+Attendance.append("brycen")
+Attendance.append("brooke")
+Attendance.append("blake")
+Attendance.append("mike")
+Attendance.append("maddox")
+Attendance.append("marshall")
+Attendance.append("marshawn")
+Attendance.append("mickey")
+Attendance.append("melissa")
+
+
+here
+
+
+
+    print("yes")
