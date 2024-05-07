1bc489e95610c8893642207f1824648d9e65fd32
Michael H <michael.hevi@vtsd.com>
2024-05-07 13:54:13 -0400

    edcfdev

--- 3.3.1 alarm.py
+++ 3.3.1 alarm.py
@@ -1,28 +1,17 @@
-Students = []
+Students = ["zaine","zay","zuko","zofia","zander","zack","ben","boolbilly", "brycen", "brooke", "blake", "Mike", "Maddox", "Marshall", "Marshawn", "Mickey", "Melissa"]
 Present = []
 Absent = []
-Students.append("zaine")
-Students.append("zay")
-Students.append("zuko")
-Students.append("zofia")
-Students.append("zander")
-Students.append ("zack")
-Students.append("ben")
-Students.append("boolbilly")
-Students.append("booboo")
-Students.append("brycen")
-Students.append("brooke")
-Students.append("blake")
-Students.append("mike")
-Students.append("maddox")
-Students.append("marshall")
-Students.append("marshawn")
-Students.append("mickey")
-Students.append("melissa")
 
 
-for student in Students:
-    here = input("who would you like to make Absent or Present")
-    if here == "yes":
-        Present.append(student)
+
+for stud in range(len(Students)):
+   print(Students[stud])
+
+input(" would you like to change anyones attendence? ")
+if "yes":
+    input(" Input Students Name ")
+else:
+
+    if ("Students" in Students):
+
 
---
+++ review/Loops.py
@@ -0,0 +1,5 @@
+# Write a loop that goes from 1 to 100
+array = [2, 4, 0, 1, 9]
+
+for number in range(len(array)):
+    print(array[number])
